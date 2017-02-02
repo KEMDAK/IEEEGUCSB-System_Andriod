4893d00a35870e430ea85c03211aa5bec0a2e536
soofyhbk2007 <mastergame_2007@hotmail.com>
2017-02-02 19:12:26 +0200

    polished the code

--- app/src/main/java/org/ieeeguc/ieeeguc/models/User.java
+++ app/src/main/java/org/ieeeguc/ieeeguc/models/User.java
@@ -1,105 +1,83 @@
 package org.ieeeguc.ieeeguc.models;
 
 import org.json.JSONObject;
-
 import java.util.Date;
+
 public class User {
-    private Type Type;
-private  String firstName;
-    private  String lastName;
-    private String email;
-    private Gender gender;
-    private Date birthdate;
-    private String ieeeMembershipID;
-    private int committeeID;
-    private String committeeName;
-    private int id;
-    private String phoneNumber;
-    private JSONObject settings;
+	private int id;
+	private Type type;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Gender gender;
+	private Date birthdate;
+	private String ieeeMembershipID;
+	private int committeeID;
+	private String committeeName;
+	private String phoneNumber;
+	private JSONObject settings;
 
 
-    public User(org.ieeeguc.ieeeguc.models.Type type, String firstName, String lastName, Gender gender, String email, Date birthdate, String ieeeMembershipID, int committeeID, String committeeName, int id, String phoneNumber, JSONObject settings) {
-        Type = type;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.email = email;
-        this.birthdate = birthdate;
-        this.ieeeMembershipID = ieeeMembershipID;
-        this.committeeID = committeeID;
-        this.committeeName = committeeName;
-        this.id = id;
-        this.phoneNumber = phoneNumber;
-        this.settings = settings;
-    }
+	public User(int id, Type type, String firstName, String lastName, Gender gender, String email, Date birthdate, String ieeeMembershipID, int committeeID, String committeeName, String phoneNumber, JSONObject settings) {
+		this.type = type;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.email = email;
+		this.birthdate = birthdate;
+		this.ieeeMembershipID = ieeeMembershipID;
+		this.committeeID = committeeID;
+		this.committeeName = committeeName;
+		this.id = id;
+		this.phoneNumber = phoneNumber;
+		this.settings = settings;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public JSONObject getSettings() {
+		return settings;
+	}
 
+	public Type getType() {
+		return Type;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
 
+	public String getLastName() {
+		return lastName;
+	}
 
-    public JSONObject getSettings() {
-        return settings;
-    }
+	public String getEmail() {
+		return email;
+	}
 
+	public Date getBirthdate() {
+		return birthdate;
+	}
 
-    public org.ieeeguc.ieeeguc.models.Type getType() {
-        return Type;
-    }
+	public Gender getGender() {
+		return gender;
+	}
 
+	public String getIeeeMembershipID() {
+		return ieeeMembershipID;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public int getCommitteeID() {
+		return committeeID;
+	}
 
-
-
-    public String getLastName() {
-        return lastName;
-    }
-
-
-
-    public String getEmail() {
-        return email;
-    }
-
-
-
-    public Date getBirthdate() {
-        return birthdate;
-    }
-
-
-
-    public Gender getGender() {
-        return gender;
-    }
-
-
-
-    public String getIeeeMembershipID() {
-        return ieeeMembershipID;
-    }
-
-
-
-    public int getCommitteeID() {
-        return committeeID;
-    }
-
-
-
-    public String getCommitteeName() {
-        return committeeName;
-    }
-
-
-
+	public String getCommitteeName() {
+		return committeeName;
+	}
 }
