5923845a8231ba37a30b76e07d9da5fb7ae9a3fb
mohamed essam <mohamed essam>
2017-02-04 00:35:55 +0200

    almost every thing is working great

--- app/src/main/java/org/ieeeguc/ieeeguc/controllers/LoginActivity.java
+++ app/src/main/java/org/ieeeguc/ieeeguc/controllers/LoginActivity.java
@@ -1,41 +1,20 @@
 package org.ieeeguc.ieeeguc.controllers;
 
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.IOException;
 
 
 import org.ieeeguc.ieeeguc.HTTPResponse;
@@ -43,21 +22,14 @@ import org.ieeeguc.ieeeguc.R;
 import org.ieeeguc.ieeeguc.models.Gender;
 import org.ieeeguc.ieeeguc.models.Type;
 import org.ieeeguc.ieeeguc.models.User;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
 
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 
-import static android.Manifest.permission.READ_CONTACTS;
 import static org.ieeeguc.ieeeguc.models.User.login;
 
 /**
@@ -78,20 +50,24 @@ public class LoginActivity extends AppCompatActivity {
         email = (AutoCompleteTextView) findViewById(R.id.email) ;
         password = (EditText) findViewById(R.id.password) ;
         send = (Button) findViewById(R.id.email_sign_in_button) ;
+        final String[] message = new String[1];
         send.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(getApplicationContext(), "waiting a reponse from the server", Toast.LENGTH_LONG).show();
                 String emailText = email.getText().toString() ;
                 String passwordText = password.getText().toString() ;
 
-                if (emailText != null && passwordText != null) {
+
+                if (emailText.length()!= 0 && passwordText.length()!= 0 ) {
                     login(emailText, passwordText, new HTTPResponse() {
 
                         @Override
                         public void onSuccess(int statusCode, JSONObject body)  {
-                            Log.i("2","");
+                            Log.i("2","it is clicked");
                             if (statusCode == 200) {
+
+                                Log.i("2","it is ok i think");
 
 
                                 try {
@@ -135,37 +111,43 @@ public class LoginActivity extends AppCompatActivity {
 
                         @Override
                         public void onFailure(int statusCode, JSONObject body) {
-                            Log.i("1","");
+                            Log.i("1","failed");
 
                             if(statusCode == 401)
                             {
-                                Toast.makeText(getApplicationContext(), "Your password or email are incorrect ", Toast.LENGTH_LONG).show();
+                                Log.i("1","your faild ");
                             }
+
                             if(statusCode == 500){
-                                Toast.makeText(getApplicationContext(), "IEEE is down", Toast.LENGTH_LONG).show();
+
                             }
                         }
                     });
                 } else {
-                    if (emailText != null) {
-                        Toast.makeText(getApplicationContext(), "You forget to write your password", Toast.LENGTH_LONG).show();
-                        Log.i("3","");
+                    if (emailText.length()!= 0) {
+
+                        Toast.makeText(getApplicationContext(), "your password is null", Toast.LENGTH_LONG).show();
+                        Log.i("2","your password is null");
+
+
                     } else {
-                        if(passwordText != null)
-                        { Toast.makeText(getApplicationContext(), "You forget to write your email", Toast.LENGTH_LONG).show();
-                        Log.i("4","");}
+                        if(passwordText.length()!= 0)
+                        {
+                            Toast.makeText(getApplicationContext(), "your email is null", Toast.LENGTH_LONG).show();
+                            Log.i("2","your email is null");
+                        }
 
                         else{
-                            Log.i("5","");
-                            Toast.makeText(getApplicationContext(), "You forget to write your email and password", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "your email and pass is null", Toast.LENGTH_LONG).show();
+                            Log.i("2","your email and pass is null");
                         }
                     }
                 }
             }
 
-    });
+        });
+            }
 
 
-}
 }
 
