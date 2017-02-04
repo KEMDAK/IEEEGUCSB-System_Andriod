1e95c1f9cdca4e13a2cd8043c0722b6b3e2b5192
Kareem Mokhtar <kareem-mokhtar@outlook.com>
2017-02-04 22:40:43 +0200

    returned the missing files

--- app/src/main/java/org/ieeeguc/ieeeguc/models/User.java
+++ app/src/main/java/org/ieeeguc/ieeeguc/models/User.java
@@ -14,8 +14,10 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+
 public class User{
 
+    public static final MediaType contentType = MediaType.parse("application/json; charset=utf-8");
     public static enum Type { ADMIN, HIGH_BOARD, MEMBER, UPPER_BOARD }
     public static enum Gender { MALE, FEMALE }
 
@@ -31,6 +33,7 @@ public class User{
     private String committeeName;
     private String phoneNumber;
     private JSONObject settings;
+
 
     public User(int id, Type type, String firstName, String lastName, Gender gender, String email, Date birthdate, String ieeeMembershipID, int committeeID, String committeeName, String phoneNumber, JSONObject settings) {
         this.type = type;
@@ -95,6 +98,64 @@ public class User{
         return committeeName;
     }
 
+    public static void ForgetPassword(String email, final HTTPResponse HTTPR) {
+        HashMap <String,String>p = new HashMap();
+        p.put("email",email) ;
+        OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url("http://ieeeguc.org/api/login")
+                .post(RequestBody.create(contentType,( new JSONObject(p)).toString()))
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) {
+                try {
+                    HTTPR.onSuccess(200,new JSONObject(response.body().toString())) ;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static void login(String email , String password ,final HTTPResponse HTTP_RESPONSE){
+        final JSONObject jsonB = new JSONObject();
+        try{
+            jsonB.put("email",email);
+            jsonB.put("password",password);
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(contentType, jsonB.toString());
+        Request request = new Request.Builder()
+                .url("http://ieeeguc.org/api/login")
+                .header("user_agent","Android")
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                try {
+                    String responseData = response.body().string();
+                    JSONObject json = new JSONObject(responseData);
+                    int x = response.code();
+                    String y = Integer.toString(x);
+                    if(y.charAt(0)== '2'){
+                        HTTP_RESPONSE.onSuccess(response.code(),json);
+                    }
+                    else{
+                        HTTP_RESPONSE.onFailure(response.code(),json);
+                    }
+                } catch (JSONException e) {
+                    HTTP_RESPONSE.onFailure(response.code(),null);}}});}
     /**
      * This method is called when the user performs an editing operation on his profile.
      * @param {String}       token            [user's token]
