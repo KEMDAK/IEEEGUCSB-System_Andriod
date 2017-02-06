15dbf3d0bf0d71687f2077ee3cf4f029cb4749a0
mohamed essam <mohamed essam>
2017-02-06 21:52:14 +0200

    done with the changes that kareem wanted

--- app/src/main/java/org/ieeeguc/ieeeguc/controllers/LoginActivity.java
+++ app/src/main/java/org/ieeeguc/ieeeguc/controllers/LoginActivity.java
@@ -30,7 +30,7 @@ import static org.ieeeguc.ieeeguc.models.User.login;
  */
 public class LoginActivity extends AppCompatActivity {
 
-    AutoCompleteTextView email ;
+    EditText email ;
     EditText password ;
     Button send ;
 
@@ -40,171 +40,170 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        email = (AutoCompleteTextView) findViewById(R.id.email) ;
+        email = (EditText) findViewById(R.id.email) ;
         password = (EditText) findViewById(R.id.password) ;
         send = (Button) findViewById(R.id.email_sign_in_button) ;
         final String[] message = new String[1];
         send.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                attemptLogin() ;
+        }});
+    }
 
-                String emailText = email.getText().toString() ;
-                String passwordText = password.getText().toString() ;
-
-
-                if (emailText.length()!= 0 && passwordText.length()!= 0 ) {
-                    login(emailText, passwordText, new HTTPResponse() {
-
-                        @Override
-                        public void onSuccess(int statusCode, JSONObject body)  {
-                            Log.i("2","it is clicked");
-                            if (statusCode == 200) {
-
-                                Log.i("2","it is ok i think");
+    public void attemptLogin() {
+        String emailText = email.getText().toString() ;
+        String passwordText = password.getText().toString() ;
 
 
-                                try {
-                                    String token = body.getString("token");
-                                    SharedPreferences Sp = getApplicationContext().getSharedPreferences(getString(R.string.shared_preferences_name), Context.MODE_PRIVATE);
-                                    SharedPreferences.Editor E = Sp.edit();
-                                    E.putString("token", token);
-                                    E.commit();
+        if (emailText.length()!= 0 && passwordText.length()!= 0 ) {
+            login(emailText, passwordText, new HTTPResponse() {
 
-                                    MainActivity.token = token ;
+                @Override
+                public void onSuccess(int statusCode, JSONObject body)  {
+
+                    if (statusCode == 200) {
 
 
 
-                                    JSONObject USer = body.getJSONObject("user") ;
-                                    int id = USer.getInt("id") ;
-                                    String TYPE = USer.getString("type") ;
-                                    Type type ;
-                                    switch (TYPE){
-                                        case "Admin" :  type = Type.ADMIN ; break ;
-                                        case "Upper Board" : type = Type.UPPER_BOARD ; break ;
-                                        case "High Board" : type = Type.HIGH_BOARD ;break ;
-                                        default:type = Type.MEMBER ; break ;
-                                    }
-                                    String FN = USer.getString("first_name") ;
-                                    String LN =USer.getString("last_name") ;
-                                    String  gender = USer.getString("gender") ;
-                                    Gender GENDER ;
-                                    switch (gender){
-                                        case "male" : GENDER = Gender.MALE ;
-                                        default: GENDER= Gender.FEMALE ;
-                                    }
-                                    String email = USer.getString("email") ;
-                                    String PN = USer.getString("phone_number") ;
-                                    String BDS = USer.getString("birthdate") ;
-                                    SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-mm-dd");
-                                    Date BD = dateFormatter.parse(BDS.substring(0,10));
-                                    String IEEE_membership_ID = USer.getString("IEEE_membership_ID") ;
-                                    JSONObject settings = USer.getJSONObject("settings") ;
 
-                                    if(USer.has("committee")  ){
-                                        JSONObject committee =USer.getJSONObject("committee") ;
-                                        String committeeName =committee.getString("committeeName") ;
-                                        int committeeID = committee.getInt("committeeID") ;
-                                        User user = new User(id,type,FN,LN,GENDER,email,BD ,IEEE_membership_ID,committeeID,committeeName,PN,settings) ;
-                                        MainActivity.loggedInUser = user ;
-                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                        startActivity(intent);}
-                                    else{
-                                        String committeeName =null;
-                                        int committeeID = 0;
-                                        User user = new User(id,type,FN,LN,GENDER,email,BD ,IEEE_membership_ID,committeeID,committeeName,PN,settings) ;
-                                        MainActivity.loggedInUser = user ;
-                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                        startActivity(intent);
-                                    }
+                        try {
+                            String token = body.getString("token");
+                            SharedPreferences Sp = getApplicationContext().getSharedPreferences(getString(R.string.shared_preferences_name), Context.MODE_PRIVATE);
+                            SharedPreferences.Editor E = Sp.edit();
+                            E.putString("token", token);
+                            E.commit();
+
+                            MainActivity.token = token ;
 
 
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
 
-                                } catch (ParseException e) {
-                                    e.printStackTrace();
+                            JSONObject JsonUser = body.getJSONObject("user") ;
+                            int id = JsonUser.getInt("id") ;
+                            String StringType = JsonUser.getString("type") ;
+                            Type type ;
+                            switch (StringType ){
+                                case "Admin" :  type = Type.ADMIN ; break ;
+                                case "Upper Board" : type = Type.UPPER_BOARD ; break ;
+                                case "High Board" : type = Type.HIGH_BOARD ;break ;
+                                default:type = Type.MEMBER ; break ;
+                            }
+                            String FN = JsonUser.getString("first_name") ;
+                            String LN =JsonUser.getString("last_name") ;
+                            String  StringGender = JsonUser.getString("gender") ;
+                            Gender gender ;
+                            switch (StringGender){
+                                case "male" : gender = Gender.MALE ; break ;
+                                default: gender= Gender.FEMALE ;
+                            }
+                            String email = JsonUser.getString("email") ;
+                            String PN = JsonUser.getString("phone_number") ;
+                            String BDS = JsonUser.getString("birthdate") ;
+                            SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-mm-dd");
+                            Date BD = dateFormatter.parse(BDS.substring(0,10));
+                            String IEEE_membership_ID = JsonUser.getString("IEEE_membership_ID") ;
+                            JSONObject settings = JsonUser.getJSONObject("settings") ;
+                            String committeeName ;
+                            int committeeID ;
+                            if(JsonUser.has("committee")  ){
+                                JSONObject committee =JsonUser.getJSONObject("committee") ;
+                                 committeeName =committee.getString("committeeName") ;
+                                 committeeID = committee.getInt("committeeID") ;
                                 }
-
+                            else{
+                                 committeeName =null;
+                                 committeeID = 0;
 
                             }
+
+                            User user = new User(id,type,FN,LN,gender,email,BD ,IEEE_membership_ID,committeeID,committeeName,PN,settings) ;
+                            MainActivity.loggedInUser = user ;
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        } catch (ParseException e) {
+                            e.printStackTrace();
                         }
 
-                        @Override
-                        public void onFailure(int statusCode, JSONObject body) {
-                            Log.i("1","failed");
+
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, JSONObject body) {
 
 
-                            if(statusCode == 401)
-                            {
-                                Log.i("1","your faild ");
-                                Snackbar.make(findViewById(R.id.email_sign_in_button), R.string.error_401,
-                                        Snackbar.LENGTH_INDEFINITE).setAction(R.string.Ok, new OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-
-                                    }
-                                })
-                                        .show();
-                            }
-
-                            if(statusCode == 500){
-
-                                Snackbar.make(findViewById(R.id.email_sign_in_button), R.string.error_500,
-                                        Snackbar.LENGTH_INDEFINITE).setAction(R.string.Ok, new OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-
-                                    }
-                                })
-                                        .show();
-
-                            }
-                        }
-                    });
-                } else {
-                    if (emailText.length()!= 0) {
-
-                        Snackbar.make(findViewById(R.id.email_sign_in_button), R.string.PasswordIsEmpty,
-                                Snackbar.LENGTH_INDEFINITE).setAction(R.string.Ok, new OnClickListener() {
+                    if(statusCode == 401)
+                    {
+                        Snackbar.make(findViewById(R.id.email_sign_in_button), "your email or password is wrong",
+                                Snackbar.LENGTH_INDEFINITE).setAction("Ok", new OnClickListener() {
                             @Override
                             public void onClick(View view) {
 
                             }
                         })
                                 .show();
-                        Log.i("2","your password is null");
+                    }
 
+                    if(statusCode == 500){
 
-                    } else {
-                        if(passwordText.length()!= 0)
-                        {
-                            Snackbar.make(findViewById(R.id.email_sign_in_button), R.string.EmailIsEmpty,
-                                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.Ok, new OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
+                        Snackbar.make(findViewById(R.id.email_sign_in_button), "IEEE is down",
+                                Snackbar.LENGTH_INDEFINITE).setAction("Ok", new OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
 
-                                }
-                            })
-                                    .show();
-                            Log.i("2","your email is null");
-                        }
+                            }
+                        })
+                                .show();
 
-                        else{
-                            Snackbar.make(findViewById(R.id.email_sign_in_button), R.string.PasswordAndEmailAreEmpty,
-                                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.Ok, new OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-
-                                }
-                            })
-                                    .show();
-                            Log.i("2","your email and pass is null");
-                        }
                     }
                 }
-            }
+            });
+        } else {
+            if (emailText.length()!= 0) {
 
-        });
+                Snackbar.make(findViewById(R.id.email_sign_in_button), "please enter your password",
+                        Snackbar.LENGTH_INDEFINITE).setAction("Ok", new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                })
+                        .show();
+
+
+            } else {
+                if(passwordText.length()!= 0)
+                {
+                    Snackbar.make(findViewById(R.id.email_sign_in_button), "please enter your email",
+                            Snackbar.LENGTH_INDEFINITE).setAction("Ok", new OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                        }
+                    })
+                            .show();
+                }
+
+                else{
+                    Snackbar.make(findViewById(R.id.email_sign_in_button), "please enter your password and email",
+                            Snackbar.LENGTH_INDEFINITE).setAction("Ok", new OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                        }
+                    })
+                            .show();
+
+                }
+            }
+        }
     }
+
 }
+
 
