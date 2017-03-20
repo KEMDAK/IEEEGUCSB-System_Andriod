3cc39837a31f5f38a73ee3ac945d3f2878ffb5f1
Abdelrahmen <abdelrahmenayman7@gmail.com>
2017-03-21 00:17:56 +0200

    Show meeting is done

--- app/src/main/java/org/ieeeguc/ieeeguc/models/Meeting.java
+++ app/src/main/java/org/ieeeguc/ieeeguc/models/Meeting.java
@@ -1,10 +1,20 @@
 package org.ieeeguc.ieeeguc.models;
 
+import android.util.Log;
+
+import org.ieeeguc.ieeeguc.HTTPResponse;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class Meeting {
 
@@ -20,6 +30,7 @@ public class Meeting {
     private User supervisor;
     private Date created_at;
     private Date updated_at;
+
 
 
 
@@ -41,8 +52,49 @@ public class Meeting {
         this.supervisor = supervisor;
         this.created_at = created_at;
         this.updated_at = updated_at;
+    }
 
+    /**
+     * This method is called when a user requests to see a meeting details.
+     * @param {int}          id            [Meeting id].
+     * @param {HTTPResponse} httpResponse  [Interface instance].
+     * @param {String}       token         [Authorization token].
+     */
+    public static void getMeeting(int id, final HTTPResponse httpResponse, String token){
 
+        String stringId = String.valueOf(id);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .header("Authorization",token)
+                .header("user_agent","Android")
+                .url("http://ieeeguc.org/api/meeting/" + stringId)
+                .get()
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+                httpResponse.onFailure(-1,null);
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                try {
+                    String body = response.body().string();
+                    JSONObject responseBody = null;
+                    responseBody = new JSONObject(body);
+                    int statusCode = response.code();
+                    httpResponse.onSuccess(statusCode,responseBody);
+                    Log.i("code",statusCode + "");
+                    Log.i("response",body);
+                } catch (JSONException e) {
+                    httpResponse.onFailure(500,null);
+                }
+                response.close();
+            }
+        });
     }
 
     public Date getStart_date() {
