1be1a1185b8c7cc5fd8b244df29cee80b67f1b30
Kareem Mokhtar <kareem-mokhtar@outlook.com>
2017-03-24 12:30:39 +0200

    Update Committee.java

--- app/src/main/java/org/ieeeguc/ieeeguc/models/Committee.java
+++ app/src/main/java/org/ieeeguc/ieeeguc/models/Committee.java
@@ -18,13 +18,6 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-import static org.ieeeguc.ieeeguc.models.User.CONTENT_TYPE;
-
-/**
- * Created by abdelrahmen on 16/02/17.
- */
-
-
 public class Committee {
 
     private static final MediaType CONTENT_TYPE = MediaType.parse("application/json; charset=utf-8");
